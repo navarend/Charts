163a988fa51328b3b1c2e6ab4fed8a44d29eae16
Roberto <navarend@gmail.com>
2018-11-12 11:47:27 -0600

    Realese charts-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
+import { LineComponent } from "./components/line/line.component";
+import { BarsComponent } from './components/bars/bars.component';
+import { DoughnutComponent } from './components/doughnut/doughnut.component';
+import { RadarComponent } from './components/radar/radar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineComponent,
+    BarsComponent,
+    DoughnutComponent,
+    RadarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
